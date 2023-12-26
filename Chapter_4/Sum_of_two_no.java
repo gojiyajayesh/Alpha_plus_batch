c81174d42ec13b39b571cb0633f51ff9f51a2a0c
jayesh-ahir <jayeshgojiya1168@gmail.com>
2023-12-27 00:29:52 +0530

    README.md Update

---
+++ Chapter_4/Sum_of_two_no.java
@@ -0,0 +1,11 @@
+public class Sum_of_two_no {
+    public static void main(String[] args) {
+        int a=12;
+        int b=34;
+        int sum=a+b;
+        System.out.println(sum);
+        // first retrive a variable value
+        // after retrive b variable value
+        // and Sum of two number and print this number's
+    }
+}
