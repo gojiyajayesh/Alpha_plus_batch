a1fe8742303e347d45527c85b0e47347a5422732
jayesh-ahir <jayeshgojiya1168@gmail.com>
2023-12-27 23:09:43 +0530

    Add Chapter-5

---
+++ Chapter_5/Ch_5_Q_1.java
@@ -0,0 +1,8 @@
+public class Ch_5_Q_1 {
+   public static void main(String[] args) {
+   int x = 2, y = 5;
+   int exp1 = (x * y / x);
+   int exp2 = (x * (y / x));
+   System.out.print(exp1 + ",");
+   System.out.print(exp2);}
+}
