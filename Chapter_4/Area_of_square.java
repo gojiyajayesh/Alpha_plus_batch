b4c5fd08305b6c4bcdd723b3233021ce95177e1a
jayesh-ahir <jayeshgojiya1168@gmail.com>
2023-12-27 21:47:40 +0530

    Add Chapter-4

---
+++ Chapter_4/Area_of_square.java
@@ -0,0 +1,11 @@
+/**
+ * Area_of_square
+ */
+import java.util.*;
+public class Area_of_square {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        float side=sc.nextFloat();
+        System.out.println(side*side);
+    }
+}
---
+++ Chapter_4/Average_of_three_number.java
@@ -0,0 +1,15 @@
+
+/**
+ * Average_of_three_number
+ */
+import java.util.Scanner;
+public class Average_of_three_number {
+
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int A=sc.nextInt();
+        int B=sc.nextInt();
+        int C=sc.nextInt();
+        System.out.println((A+B+C)/3);
+    }
+}
