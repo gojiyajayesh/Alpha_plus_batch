79e4df8cdb4d61d310ac7a74fe17312e5af4f019
jayesh-ahir <jayeshgojiya1168@gmail.com>
2023-12-28 23:05:51 +0530

    Add Chapter-6

---
+++ Chapter_6/week.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class week {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter Number");
+        int week=sc.nextInt();
+        switch(week)
+        {
+            case 1:
+            System.out.println("Monday");
+            break;
+            case 2:
+            System.out.println("Tuesday");
+            break;
+            case 3:
+            System.out.println("wednesday");
+            break;
+            case 4:
+            System.out.println("thrusday");
+            break;
+            case 5:
+            System.out.println("friday");
+            break;
+            case 6:
+            System.out.println("saturday");
+            break;
+            case 7:
+            System.out.println("Sunday");
+            break;
+            default:
+            System.out.println("Please Enter Valid Number");
+        }
+    }
+}
