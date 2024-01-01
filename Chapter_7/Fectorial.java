c656f87e003bb682b26416a09dc9b46f0b406a80
Jayesh Gojiya <jayeshgojiya1168@gmail.com>
2024-01-01 22:16:50 +0530

    Add Chapter-7

--- Chapter_7/Fectorial.java
+++ Chapter_7/Fectorial.java
@@ -1,5 +1,5 @@
 import java.util.*;
-public class Fectorial{
+public class Fectorial {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter a Number you find");
