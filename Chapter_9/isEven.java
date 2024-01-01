a56eb8e00f6bad3c22ba623615f65f770f7942ef
Jayesh Gojiya <jayeshgojiya1168@gmail.com>
2024-01-01 22:40:43 +0530

    Add Chapter-9

---
+++ Chapter_9/isEven.java
@@ -0,0 +1,12 @@
+public class isEven {
+    public static void main(String[] args) {
+        System.out.println(isEVen(12));
+        System.out.println(isEVen(15));
+    }
+    public static boolean isEVen(int number)
+    {
+        if(number%2==0)
+        return true;
+        return false;
+    }
+}
