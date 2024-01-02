13ad38aa6f37f882b5de7f966f167c4eba1017c8
Jayesh Gojiya <jayeshgojiya1168@gmail.com>
2024-01-02 22:18:28 +0530

    Update Foldare name

--- Chapter_10/Raj.java
+++ Chapter_10/Raj.java
@@ -2,9 +2,20 @@ public class Raj {
     public static void main(String[] args) {
         print(11);
     }
-
     public static void print(int row) {
         for (int i = 0; i <= row; i++) {
+            for (int j = 1; j <= row - i; j++) {
+                System.out.print("\t");
+            }
+            for (int j = i; j >= 1; j--) {
+                System.out.print(j+"\t");
+            }
+            for (int j = 2; j <= i; j++) {
+                System.out.print(j+"\t");
+            }
+            System.out.println();
+        }
+        for (int i = row; i >=1; i--) {
             for (int j = 1; j <= row - i; j++) {
                 System.out.print("\t");
             }
