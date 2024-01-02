d0c9712ff44963aec3099c306bff055f1497d0da
Jayesh Gojiya <jayeshgojiya1168@gmail.com>
2024-01-02 22:23:23 +0530

    Update Foldar name

--- Chapter_10/Butter_fly.java
+++ Chapter_10/Butter_fly.java
@@ -6,7 +6,6 @@ public class Butter_fly {
     public static void main(String[] args) {
         print(8);
     }
-
     public static void print(int row) {
         for (int i = 1; i <= row; i++) {
             for (int j = 1; j <= i; j++)
